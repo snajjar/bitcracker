79c81edd5b2988196dbaba7c2cdc86f7f5edc344
Sébastien Najjar <sebastien.najjar@gmail.com>
2020-03-29 18:32:30 +0200

    clean up interface and make changeable interval

--- web/client/src/index.js
+++ web/client/src/index.js
@@ -7,15 +7,10 @@ Vue.use(VueRouter);
 
 import $ from 'jquery';
 import Routes from './routes.js';
-
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
+import fontawesome from '@fortawesome/fontawesome-free';
 
 $(document).ready(() => {
     const app = new Vue({
         router: Routes
     }).$mount('#app')
 });
-
-// Now the app has started!
--- web/client/src/main.vue
+++ web/client/src/main.vue
@@ -1,19 +1,31 @@
 <style>
 html,
 body {
-    min-height: 100vh;
+    height: calc(100vh - 30px);
     background: #0a0224;
     color: white;
+    margin: 0;
+    padding: 10px;
 }
 
 .main {
-    height: calc(100vh - 42px);
+    height: calc(100vh - 100px);
 }
 
 .chart {
     max-width: 100%;
     max-height: calc(100vh - 300px);
     height: calc(100vh - 300px);
+}
+
+.tools {
+    margin-left: 10px;
+    margin-bottom: 20px;
+}
+
+.refreshing {
+    display: inline-block;
+    margin-left: 10px;
 }
 </style>
 
@@ -25,6 +37,24 @@ body {
             <router-link to="/bar">Go to Bar</router-link>
         </p>
         -->
+        <div class="tools">
+            <label><i class="fas fa-spinner"></i> Interval:</label>
+            <select v-model="intervalStr">
+                <option value="1" selected>1m</option>
+                <option value="5">5m</option>
+                <option value="15">15m</option>
+                <option value="30">30m</option>
+                <option value="60">1h</option>
+                <option value="240">4h</option>
+                <option value="1440">1d</option>
+                <option value="10080">7d</option>
+                <option value="21600">15d</option>
+            </select>
+
+            <div class="refreshing" v-if="refreshing">
+                <span><i class="fas fa-spinner fa-spin"></i> refreshing...</span>
+            </div>
+        </div>
         <div class="chart">
             <financial-chart :series="series" :options="chartOptions"></financial-chart>
         </div>
@@ -38,7 +68,6 @@ import moment from "moment";
 import FinancialChartVue from "./components/charts/FinancialChart.vue";
 
 export default {
-    created: function() {},
     components: {
         "financial-chart": FinancialChartVue
     },
@@ -62,23 +91,44 @@ export default {
                     }
                 }
             };
-        }
+        },
+        interval: function() {
+            return parseInt(this.intervalStr, 10);
+        },
+    },
+    created: function() {
+        this.currentDataInterval = 1; // last interval for which we fetched data
+        this.setRefreshInterval();
     },
     mounted() {
         this.refreshData();
-
-        // refresh every minute
-        setInterval(() => {
+    },
+    updated() {
+        if( this.currentDataInterval !== this.interval ) {
             this.refreshData();
-        }, 60000);
+            this.setRefreshInterval();
+        }
     },
     data() {
         return {
-            maxCandleDisplay: 80, // number of candle to display
-            series: []
+            maxCandleDisplay: 100, // number of candle to display
+            series: [],
+            intervalStr: "1", // in minutes
+
+            refreshing: false,
         };
     },
     methods: {
+        setRefreshInterval: function() {
+            // update the refresh rate depending on the period
+            if( this.refreshInterval !== null ) {
+                clearInterval(this.refreshInterval);
+            }
+            this.refreshInterval = setInterval(() => {
+                this.refreshData();
+            }, this.interval * 60000);
+        },
+
         secureFetch: function(apiURL) {
             return new Promise((resolve, reject) => {
                 const CORSProxy = "https://cors-anywhere.herokuapp.com"; // needed to handle CORS issue
@@ -104,9 +154,11 @@ export default {
         },
 
         refreshData: async function() {
-            const apiURL =
-                "https://api.kraken.com/0/public/OHLC?pair=BTCEUR&interval=1";
+            console.log("Refreshing " + moment().format("LLL"));
+            this.refreshing = true;
+            const apiURL = "https://api.kraken.com/0/public/OHLC?pair=BTCEUR&interval=" + this.interval;
             const json = await this.secureFetch(apiURL);
+            this.refreshing = false;
             const arr = _.get(json, ["result", "XXBTZEUR"]);
             const now = moment();
 
@@ -119,7 +171,7 @@ export default {
                 // check if we wanna display it
                 let duration = moment.duration(now.diff(d));
                 let minutesDiff = duration.asMinutes();
-                if (minutesDiff < this.maxCandleDisplay) {
+                if (minutesDiff / this.interval < this.maxCandleDisplay) {
                     // then, get open, high, low and close values
                     let values = [
                         parseFloat(r[1]),
@@ -133,6 +185,7 @@ export default {
                 }
             });
 
+            this.currentDataInterval = this.interval;
             this.series = [{ data: serie }];
         }
     }
