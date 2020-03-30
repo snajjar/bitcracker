c006b0c05773103934ad50c8bdda4b05a9209dd1
Sébastien Najjar <sebastien.najjar@gmail.com>
2020-03-30 07:24:59 +0200

    3-layer model

--- ml/train.js
+++ ml/train.js
@@ -25,16 +25,24 @@ const nbDataByPeriod = 6;
 const nbDataInput = nbPeriods * nbDataByPeriod;
 
 // The model will output a prediction for the next period
-// - open price
 // - high price
 // - low price
-// - close price
-const nbDataOutput = 4;
+// - direction (SELLING or BUYING): is the open price > close price
+const nbDataOutput = 3;
+// Other ideas:
+// - open price // DISABLED
+// - close price // DISABLED
+
 
 // thoses values will have to be convert back from sigmoid (using logit function)
 const model = tf.sequential({
     layers: [
         tf.layers.dense({ inputShape: [nbDataInput], units: nbDataInput, activation: 'sigmoid' }),
+        tf.layers.dropout(0.8),
+        tf.layers.dense({ units: Math.round(nbDataInput * 0.5), activation: 'sigmoid' }),
+        tf.layers.dropout(0.8),
+        tf.layers.dense({ units: Math.round(nbDataInput * 0.25), activation: 'sigmoid' }),
+        tf.layers.dropout(0.8),
         tf.layers.dense({ units: nbDataOutput, activation: 'sigmoid' }),
     ]
 });
@@ -44,30 +52,6 @@ model.compile({
     loss: 'meanSquaredError',
     metrics: ['accuracy']
 });
-
-const splitData = function(data) {
-    // split our data into train data and test data
-    let l = data.length;
-    let limit = Math.round(data.length * 0.8);
-    const trainData = data.slice(0, limit); // use 80% of our data to train
-    const testData = data.slice(limit);
-
-    console.log('[*] splitting data:');
-    console.log(`[*]   train sample: size ${trainData.length}`);
-    console.log(`[*]   test sample: size ${testData.length}`);
-
-    return [trainData, testData];
-}
-
-const debug = function(o) {
-    console.log(require('util').inspect(o));
-}
-
-const predict = function(periods) {
-    const inputs = tf.tensor(periods);
-    let outputs = model.predict(inputs);
-    outputs.print();
-}
 
 const train = async function(data) {
     let inputs = [];
@@ -93,13 +77,15 @@ const train = async function(data) {
 
         let outputData = data[i + nbPeriods];
         outputs.push([
-            outputData.open,
+            //outputData.open,
             outputData.high,
             outputData.low,
-            outputData.close,
+            outputData.open > outputData.close ? 0 : 1
         ]);
         nbSamples++;
     }
+
+    console.log(`[*] training AI on ${nbSamples} samples`);
 
     // build our tensors
     //debug(inputs);
@@ -113,20 +99,23 @@ const train = async function(data) {
     // train the model for each tensor
     const response = await model.fit(inputTensor, outputTensor, {
         shuffle: true,
-        epochs: 50,
+        epochs: 40,
         batchsize: 50,
-        validtionSplit: 0.2
+        validtionSplit: 0.25
     });
+
+    tf.dispose(inputTensor);
+    tf.dispose(outputTensor);
 }
 
 
 const main = async function() {
     // load data from CSV
     const btcData = await csv.fetchData('../data/btceur/Kraken_BTCEUR_1h.csv');
-    //const [trainData, testData] = splitData(btcData);
 
-    train(btcData);
-    //test(testData);
+    await train(btcData);
+
+    await model.save('file://./models/supervised/Kraken_BTCEUR_1h/');
 }
 
 main();
