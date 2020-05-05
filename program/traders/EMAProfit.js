7175e0e36557d9fea1787c3d3a2b2cda8fcec879
Sébastien Najjar <sebastien.najjar@gmail.com>
2020-05-05 11:16:01 +0200

    improve EMAProfit

--- program/traders/EMAProfit.js
+++ program/traders/EMAProfit.js
@@ -12,6 +12,11 @@ class EMAProfitTrader extends Trader {
         this.emaDownTrigger = 0.2;
         this.maxTimeInTrade = 1440 * 1.5; // 1 day
         this.objective = 0.12;
+        // this.adxPeriods = 14;
+        // this.adxMinTrigger = 7;
+        // this.adxMaxTrigger = 90;
+        this.bbandTrigger = 0.0035;
+        this.volatilityTrigger = 0.0042;
 
         // trade decision making
         this.inTrade = false;
@@ -47,6 +52,47 @@ class EMAProfitTrader extends Trader {
         });
     }
 
+    getADX(dataPeriods) {
+        let highPrices = _.map(dataPeriods, p => p.high);
+        let lowPrices = _.map(dataPeriods, p => p.low);
+        let closePrices = _.map(dataPeriods, p => p.close);
+        return new Promise((resolve, reject) => {
+            tulind.indicators.adx.indicator([highPrices, lowPrices, closePrices], [this.adxPeriods], function(err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results[0]);
+                }
+            });
+        });
+    }
+
+    getRSI(dataPeriods) {
+        let closePrices = _.map(dataPeriods, p => p.close);
+        return new Promise((resolve, reject) => {
+            tulind.indicators.rsi.indicator([closePrices], [14], function(err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    }
+
+    getBBands(dataPeriods) {
+        let closePrices = _.map(dataPeriods, p => p.close);
+        return new Promise((resolve, reject) => {
+            tulind.indicators.bbands.indicator([closePrices], [40, 2], function(err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve([results[0], results[1], results[2]]);
+                }
+            });
+        });
+    }
+
     // decide for an action
     async action(dataPeriods, currentBitcoinPrice) {
         // let stopped = this.stopLoss(this.stopLossRatio);
@@ -63,7 +109,29 @@ class EMAProfitTrader extends Trader {
         let bigUp = diff > this.emaUpTrigger;
 
         if (!this.inTrade) {
-            if (bigDown) {
+            // let adx = await this.getADX(dataPeriods);
+            // let lastADX = adx[adx.length - 1];
+
+            // // if adx is broken, don't filter
+            // let trendSeemsGood = isNaN(lastADX) || (this.adxMinTrigger <= lastADX && lastADX <= this.adxMaxTrigger);
+
+            // validate that momentum seems right, we are not buying when going into oversold
+            let rsi = await this.getRSI(dataPeriods);
+            let lastRSI = rsi[0][rsi[0].length - 1];
+            let overbought = lastRSI > 68;
+
+            // get Bollinger bands, estimate the volatility
+            let [lowBand, midBand, highBand] = await this.getBBands(dataPeriods);
+            let vol = [];
+            for (var i = 0; i < lowBand.length; i++) {
+                let volatility = ((highBand[i] - midBand[i]) + (midBand[i] - lowBand[i])) / midBand[i];
+                vol.push(volatility);
+            }
+            let avgVolatility = _.mean(vol);
+            let priceIsVolatile = avgVolatility > this.volatilityTrigger;
+
+
+            if (bigDown && !overbought && priceIsVolatile) {
                 // BUY condition
                 this.timeInTrade = 0;
                 return this.buy();
